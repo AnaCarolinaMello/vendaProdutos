fcbb8158cb42855e8b2ab2b06fd7ec8fabc1c795
AnaCarolinaMello <caldasdemelloanacarolina@gmail.com>
2023-05-20 11:00:48 -0300

    feat: adiociona o header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 import Main from './components/Main'
 import Pesquisa from './components/Pesquisa'
 import Detalhes from './components/Detalhes'
-import Header from './components/Header'
 
 import {
   Routes,
@@ -20,7 +19,6 @@ function App() {
       <Routes>
         <Route path='/vendaProdutos' element={<Main />} />
         <Route path='/vendaProdutos/pesquisa' element={<Pesquisa />} />
-        <Route path='/vendaProdutos/header' element={<Header />} />
         <Route path="/vendaProdutos/delathes/:id?" element={<Detalhes />} />
       </Routes>
     </>
